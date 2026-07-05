5608b40ccdd0bec0ecb92d8de4a89b9aa81b572b
Xiaoyu0601-Wang <micheal.wang@hotmail.com>
2026-07-05 21:12:17 +0900

    chore: remove a definition

--- src/asr_sdm_universe/planning/asr_sdm_planning_manager/include/asr_sdm_planning_manager/topo_replan_fsm.h
+++ src/asr_sdm_universe/planning/asr_sdm_planning_manager/include/asr_sdm_planning_manager/topo_replan_fsm.h
@@ -61,8 +61,7 @@ private:
 
   /* ROS utils */
   std::shared_ptr<rclcpp::Node> node_;
-  rclcpp::TimerBase::SharedPtr exec_timer_, safety_timer_, vis_timer_, frontier_timer_,
-    map_update_timer_;
+  rclcpp::TimerBase::SharedPtr exec_timer_, safety_timer_, vis_timer_, frontier_timer_;
   rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr waypoint_sub_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
   rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr replan_pub_, new_pub_;
